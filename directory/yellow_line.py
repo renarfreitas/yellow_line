4dea694254ea3d0d672e4c8b02c979edee645327
Renar Freitas <renarfreitas@gmail.com>
2022-01-27 21:54:14 -0300

    Face detection
    try face detection with python and opencv.

--- directory/yellow_line.py
+++ directory/yellow_line.py
@@ -1,13 +1,38 @@
 import cv2
-
-cap = cv2.VideoCapture()
+cap = cv2.VideoCapture(0)
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eyesCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+smileCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_smile.xml')
 
 while True:
     _, frame = cap.read()
 
-    cv2.imshow("detections", frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    if cv2.waitKey(1) == 27:
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor = 1.1,
+        minNeighbors = 5,
+        minSize = (30, 30)
+    )
+
+    #Face detection
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
+    
+    #Eyes detection
+    eyes = eyesCascade.detectMultiScale(gray, 1.2, 18)
+    for (x, y, w, h) in eyes:
+            cv2.rectangle(frame, (x,y), (x+w, y+h), (255, 0, 0), 2)
+
+    #Smiles
+    smiles = smileCascade.detectMultiScale(gray, 1.7, 20)
+    for (x, y, w, h) in smiles:
+            cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 0, 255), 2)
+
+    cv2.imshow("video", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
